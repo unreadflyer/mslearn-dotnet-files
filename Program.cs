f8d17a9f647bd6d817b9dec76cc998cd748d72f9
unreadflyer <unr34d@gmail.com>
2025-12-11 08:56:21 +0100

    no writeLine un function

--- Program.cs
+++ Program.cs
@@ -5,18 +5,17 @@ using System.Reflection.Metadata.Ecma335;
 
 Console.WriteLine("Hello, World!");
 
-int coderreur;
-coderreur =findFiles("stores"); 
-
- int findFiles (string FolderName) {
-
-    var foundFiles= System.IO.Directory.GetDirectories(FolderName);
-
+    IEnumerable<string>  foundFiles = FindFiles("stores");
     foreach (string folder in foundFiles)
     {
         Console.WriteLine (folder);
     }
 
-    return 0;
+
+
+ IEnumerable<string> FindFiles (string FolderName) {
+
+    var foundFiles= System.IO.Directory.GetDirectories(FolderName);
+    return foundFiles;
  
 }
