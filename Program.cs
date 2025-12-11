f50d6fa46dcf2dc4f4d88e18441c398101694483
unreadflyer <unr34d@gmail.com>
2025-12-11 09:31:49 +0100

    enumarate files instead of directories

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO;
 using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
+
 
 Console.WriteLine("Hello, World!");
 
-    IEnumerable<string>  foundFiles = FindFiles("stores");
-    foreach (string folder in foundFiles)
-    {
-        Console.WriteLine (folder);
-    }
-
-
-
- IEnumerable<string> FindFiles (string folderName) {
-
-    var foundFiles = System.IO.Directory.EnumerateDirectories(folderName);
-    return foundFiles;
- 
+IEnumerable<string>  foundFiles = FindFiles("stores");
+foreach (string folder in foundFiles)
+{
+    Console.WriteLine (folder);
 }
+
+ IEnumerable<string> FindFiles (string folderName)
+ {
+    var foundFiles = System.IO.Directory.EnumerateFiles(folderName);
+    return foundFiles;
+ }
