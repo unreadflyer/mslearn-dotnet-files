2f32b66e6256d1f336b8e7d02fe83fd477eace52
unreadflyer <unr34d@gmail.com>
2025-12-11 08:32:48 +0100

    premier jet fonctionnel

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+
+int coderreur;
+coderreur =findFiles("stores"); 
+
+ int findFiles (string FolderName) {
+
+    var foundFiles= System.IO.Directory.GetDirectories(FolderName);
+
+    foreach (string folder in foundFiles)
+    {
+        Console.WriteLine (folder);
+    }
+
+    return 0;
+ 
+}
